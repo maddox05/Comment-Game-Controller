732150158ab1c2fd4d10fcc491f40bfa9d929181
maddox2 <maddoxdesktopfake@admin.admin>
2023-06-09 23:31:22 -0400

    added yt functionality

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import time
 import keyboard
 from TikTokLive import TikTokLiveClient
 from TikTokLive.types.events import CommentEvent
+import pytchat
+import threading
 
 tiktok_username = input("Type your username: ")
 tiktok_client = TikTokLiveClient(unique_id="@" + tiktok_username)
+
+yt_url = input("Type your video url (end id): ")
+yt_chat = pytchat.create(video_id=yt_url)
 
 key_events = {"up", "down", "left", "right", "a", "b"}
 
 
 @tiktok_client.on("comment")
-async def on_comment(event: CommentEvent):
+async def on_ttcomment(event: CommentEvent):
     try:
         keypress = event.comment.lower()
         if keypress in key_events:
@@ -24,7 +29,25 @@ async def on_comment(event: CommentEvent):
         quit()
 
 
+def on_ytcomment():
+    while yt_chat.is_alive():
+        for i in yt_chat.get().sync_items():
+            try:
+                keypress = i.message.lower()
+                if keypress in key_events:
+                    keyboard.press(keypress)
+                    time.sleep(.3)  # await asyncio.sleep(.3)
+                    keyboard.release(keypress)
+                    print(f"{i.author.name} -> {i.message}")
+            except Exception as err:
+                print(f"{err}\n Quitting now")
+                time.sleep(4)
+                quit()
+
+
 if __name__ == "__main__":
+    thread1 = threading.Thread(target=on_ytcomment())
+    thread1.start()
     tiktok_client.run()
 
 # add ability to merge tiktok and youtube comments into one.
